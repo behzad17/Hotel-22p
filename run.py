38e280043ec62db8901e3628e8f38a04e512a8b4
BJ <158008244+behzad17@users.noreply.github.com>
2024-10-26 15:28:58 +0000

    fix some typing errors on the code

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class HotelManagement:
       
     def display_rooms(self):
         print("Available rooms in the hotel:")
-        for room in self.room:
+        for room in self.rooms:
             print(room)
 
         if __name__ == "__main__":
@@ -31,13 +31,13 @@ class HotelManagement:
             check_out = datetime.date(2024, 10, 30)
             hotel.display_available_rooms() 
             hotel.make_reservation("Behzad Javadian", "Room 101", check_in, check_out)
-            hotel.check_out_guest("Room !01")
+            hotel.check_out_guest("Room 101")
             hotel.display_available_rooms() 
 
 
     #function to available rooms
     def display_available_rooms(self):
-        available_rooms = [room for room in self.rooms if room not in self reservations]
+        available_rooms = [room for room in self.rooms if room not in self.reservations]
         if available_rooms:
             print("Available rooms:")
             for room in available_rooms:
@@ -48,7 +48,7 @@ class HotelManagement:
 
     #make_reservation
     def make_reservation(self, name, room, check_in, check_out):
-        if room in self.room and room not in self.reservation:
+        if room in self.room and room not in self.reservations:
             self.reservation[room] = {
                 "name": name,
                 "check_in": check_in,
@@ -65,3 +65,6 @@ class HotelManagement:
             print(f"Guest ckecked out from {room}")
         else:
             print(f"Room {room} is not currently reserved")
+
+    
+    
