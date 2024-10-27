954705f8d59d67585878e3a6a7f71573f38842fe
BJ <158008244+behzad17@users.noreply.github.com>
2024-10-27 12:39:33 +0000

    add def to update rooms worksheet

--- run.py
+++ run.py
@@ -66,7 +66,21 @@ class HotelManagement:
         else:
             print(f"Room {room} is not currently reserved")
 
-    
+    def update_rooms_worksheet(data):
+        """
+        Update sales worksheet, add new row with the list data provided
+        """
+        print("Updating sales worksheet...\n")
+        sales_worksheet = SHEET.worksheet("sales")
+        sales_worksheet.append_row(data)
+        print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
+
 rooms = SHEET.worksheet("rooms")    
 data = rooms.get_all_values()
 print(data)
